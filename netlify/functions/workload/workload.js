0d9ccc29b549f118e5228100c659eabcfe56ca1e
ed-sa-ma <28660051+ed-sa-ma@users.noreply.github.com>
2021-03-20 11:04:55 +1100

    Return total workload across all cpus

--- netlify/functions/workload/workload.js
+++ netlify/functions/workload/workload.js
@@ -8,15 +8,13 @@ exports.handler = async function workload(event) {
   }
 
   try {
-    let numCpus = os.cpus().length;
     let load1min = os.loadavg()[0];
-    let loadAvg1min = (load1min / numCpus).toFixed(4);
 
     let time = new Date();
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ load: loadAvg1min, time }),
+      body: JSON.stringify({ load: load1min, time }),
       headers: { "Content-type": "application/json" }
     };
   } catch (err) {
